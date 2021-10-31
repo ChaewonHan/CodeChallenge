a239fce682bfe099325ea84c71f2d119fa36128e
ChaewonHan <chwonhan00@gmail.com>
2021-11-01 00:21:02 +0900

    #3 test: 회원가입 - 성공 테스트 BDD 적용

---
+++ src/test/java/com/project/challenge/repository/UserServiceTest.java
@@ -0,0 +1,61 @@
+package com.project.challenge.repository;
+
+import com.project.challenge.domain.user.User;
+import com.project.challenge.dto.UserDto;
+import com.project.challenge.service.user.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    //테스트용 데이터
+    final String userId = "test";
+    final String password = "test!";
+    final String email = "coding@gmail.com";
+    final String username = "테스트";
+    final UserDto userDto = new UserDto(userId, password, email, username);
+
+    User testUserEntity(UserDto userDto) {
+        return User.builder()
+                .userNo(1L)
+                .userId(userDto.getUserId())
+                .username(userDto.getUsername())
+                .password(userDto.getPassword())
+                .email(userDto.getEmail())
+                .deleteAccount(false)
+                .build();
+    }
+
+
+    @Test
+    @DisplayName("회원가입 - 성공")
+    void save() {
+        //given
+        User user = testUserEntity(userDto);
+        given(userRepository.save(any(User.class))).willReturn(user);
+
+        //when
+        String saveId = userService.userSave(userDto);
+
+        //then
+        then(userRepository).should(times(1)).existsByUserIdAndDeleteAccount(userId, false);
+        then(userRepository).should(times(1)).existsByUsernameAndDeleteAccount(username, false);
+        then(userRepository).should(times(1)).existsByEmailAndDeleteAccount(email, false);
+
+    }
+
+}
