07a5e6c38da02368d970e73a70d7a24dabc55ed1
ChaewonHan <chwonhan00@gmail.com>
2021-12-02 00:58:23 +0900

    #7 feature: AwsS3Config 추가

--- src/main/java/com/project/challenge/CodingChallengeApplication.java
+++ src/main/java/com/project/challenge/CodingChallengeApplication.java
@@ -3,11 +3,10 @@ package com.project.challenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
+@SpringBootApplication
 public class CodingChallengeApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
